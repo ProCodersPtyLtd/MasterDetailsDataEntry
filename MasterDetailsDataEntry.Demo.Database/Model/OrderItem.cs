58192fc454dfa031b9f2be9157e9b44e315a0c80
Ev <ev.uklad@procoders.com.au>
2020-10-24 23:27:13 +1100

    added ef core to sql

---
+++ MasterDetailsDataEntry.Demo.Database/Model/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MasterDetailsDataEntry.Demo.Database.Model
+{
+    public partial class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string ItemName { get; set; }
+        public int Qty { get; set; }
+        public bool? IsMajor { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
--- MasterDetailsDataEntry.Tests/UnitTest1.cs
+++ MasterDetailsDataEntry.Tests/UnitTest1.cs
@@ -1,6 +1,11 @@
+using MasterDetailsDataEntry.Demo.Database.Model;
 using MasterDetailsDataEntry.Shared;
 using MasterDetailsDataEntry.Shared.Forms;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace MasterDetailsDataEntry.Tests
@@ -18,6 +23,38 @@ namespace MasterDetailsDataEntry.Tests
 
             var provider = new DataEntryProvider();
             var r = provider.GetFormFields(new MyForm());
+        }
+
+        [Fact]
+        public void DbAccessTest()
+        {
+            using (var db = new TestContext())
+            {
+                object order = new Order { ClientName = "test", CreateDate = DateTime.Now, ExecuteDate = DateTime.Now };
+                db.Add(order);
+                db.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void DbAccessReadTest()
+        {
+            var type = typeof(Order);
+            var type2 = typeof(OrderItem);
+
+            using (var db = new TestContext())
+            {
+                var obj = db.Find(type, 1);
+                var navigation = db.Entry(obj).Navigations.FirstOrDefault(n => n.Metadata.Name == type2.Name);
+                navigation?.Load();
+                var l2 = navigation?.CurrentValue as IEnumerable;
+                //var l3 = new List<object>();
+                //l3.AddRange()
+                IList items = l2.Cast<object>().ToList();
+
+
+                var list = (obj as Order).OrderItem.ToList();
+            }
         }
     }
 
