8282d60134b60be8ec72c3bcf17c3caa8df0e675
Ev <ev.uklad@procoders.com.au>
2020-12-14 23:07:40 +1100

    added simple rules

---
+++ Platz.ObjectBuilder.Blazor/Controllers/Validation/Rules/FromTablesNotEmpty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platz.ObjectBuilder.Blazor.Controllers.Validation.Rules
+{
+    public class FromTablesNotEmpty : IObjectBuilderRule
+    {
+        public RuleValidationResult Validate(IQueryModel qm)
+        {
+            if (!qm.FromTables.Any())
+            {
+                return new RuleValidationResult("From clause should contain at list one table");
+            }
+
+            return null;
+        }
+    }
+}
