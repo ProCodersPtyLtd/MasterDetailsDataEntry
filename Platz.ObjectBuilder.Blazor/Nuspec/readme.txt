4efd6fa892c4a188ad54d61d465058f8a4571bd8
Ev <ev.uklad@procoders.com.au>
2020-12-31 08:46:52 +1100

    fixed save validation

--- Platz.ObjectBuilder.Blazor/Nuspec/readme.txt
+++ Platz.ObjectBuilder.Blazor/Nuspec/readme.txt
@@ -24,7 +24,7 @@ Installation process.
 
 5. Create new folder named [StoreData] in your project
 
-6. Add the following references to jquery and bootstrap jsut befor your styles:
+6. Add the following references to jquery and bootstrap just before your styles:
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
---
+++ SqlForms.Demo/MyForms/MyCustomerHeaderForm.cs
@@ -0,0 +1,29 @@
+﻿using MasterDetailsDataEntry.Demo.Database;
+using MasterDetailsDataEntry.Demo.Database.AdventureWorks;
+using Platz.SqlForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqlForms.Demo.MyForms
+{
+    public class MyCustomerHeaderForm : DynamicEditFormBase<AdventureWorksContext>
+    {
+        protected override void Define(DynamicFormBuilder builder)
+        {
+            builder.Entity<Customer>(e =>
+            {
+                e.ExcludeAll();
+
+                e.Property(p => p.CustomerId).IsReadOnly();
+
+                e.Property(p => p.FirstName).IsReadOnly();
+
+                e.Property(p => p.MiddleName).IsReadOnly();
+
+                e.Property(p => p.LastName).IsReadOnly();
+            });
+        }
+    }
+}
