47b221641581c33c48158afa57f852c9183fedb2
Ev <ev.uklad@procoders.com.au>
2021-06-06 21:07:59 +1000

    adding Join edit modal

--- Platz.ObjectBuilder/Schema/ComponentModel/QueryFromProperty.cs
+++ Platz.ObjectBuilder/Schema/ComponentModel/QueryFromProperty.cs
@@ -7,36 +7,24 @@ namespace Platz.ObjectBuilder.Schema
 {
     public class QueryFromProperty
     {
-        public string Name { get; private set; }
-        //public string OriginalName { get; private set; }
-        public bool Pk { get; private set; }
-        public bool Fk { get; private set; }
-
-        public StoreProperty OriginalStoreProperty { get; set; }
-
         // component fields
-        // ToDo: Alias should be removed as it is not used, only in tests
         public string Alias { get; set; }
         public bool Selected { get; set; }
+        public string Name { get { return OriginalStoreProperty.Name; } }
+        public bool Pk { get { return OriginalStoreProperty.Pk; } }
+        public bool Fk { get { return OriginalStoreProperty.Fk; } }
+        public StoreProperty OriginalStoreProperty { get; set; }
 
         public QueryFromProperty() { }
 
         public QueryFromProperty(StoreProperty source)
         {
             OriginalStoreProperty = source;
-            Name = source.Name;
-            Pk = source.Pk;
-            Fk = source.Fk;
-            //StoreProperty = source;
         }
 
         public QueryFromProperty(QuerySelectProperty source)
         {
             OriginalStoreProperty = new SubQueryStoreProperty(source);
-            //OriginalStoreProperty = source.StoreProperty;
-            //OriginalName = source.StoreProperty.Name;
-            Name = source.OutputName;
-            //StoreProperty = source;
         }
     }
 }
