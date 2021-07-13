00cb10d1ce69f8c0e92cb38c32f6eca17aabce59
Ev <ev.uklad@procoders.com.au>
2021-07-13 21:18:09 +1000

    updated platz data service t4 generator to support group by

--- Platz.SqlForms.Shared/FormBuilders/DataServiceFormBuilder.cs
+++ Platz.SqlForms.Shared/FormBuilders/DataServiceFormBuilder.cs
@@ -8,10 +8,15 @@ namespace Platz.SqlForms
     public class DataServiceFormBuilder : FormBuilder
     {
         public Func<object[], object> ListQuery { get; private set; }
+        public Func<QueryOptions, object[], object> ListQueryOptions { get; private set; }
 
         public virtual void SetListMethod(Func<object[], object> method)
         {
             ListQuery = method;
+        }
+        public virtual void SetListMethod(Func<QueryOptions, object[], object> method)
+        {
+            ListQueryOptions = method;
         }
     }
 }
