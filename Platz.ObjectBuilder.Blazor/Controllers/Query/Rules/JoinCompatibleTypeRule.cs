8c84999a6b507efa5e22df6e2435de58822446cd
Ev <ev.uklad@procoders.com.au>
2021-07-25 13:52:28 +1000

    added join props type check

---
+++ Platz.ObjectBuilder.Blazor/Controllers/Query/Rules/JoinCompatibleTypeRule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Platz.ObjectBuilder.Schema;
+
+namespace Platz.ObjectBuilder.Blazor.Validation.Rules
+{
+    public class JoinCompatibleTypeRule : SubQueryBuilderRuleBase
+    {
+        public override RuleValidationResult Validate(IQueryModel qm)
+        {
+            var badJoins = new List<string>();
+            var joins = qm.FromTableJoins.Where(j => !j.IsDeleted);
+
+            foreach (var join in joins)
+            {
+                var leftObject = qm.FromTables.First(t => t.Alias == join.Source.LeftObjectAlias);
+                var leftProp = leftObject.Properties.First(p => p.Name == join.Source.LeftField).OriginalStoreProperty;
+                var rightObject = qm.FromTables.First(t => t.Alias == join.Source.RightObjectAlias);
+                var rightProp = rightObject.Properties.First(p => p.Name == join.Source.RightField).OriginalStoreProperty;
+
+                if (leftProp.Type != rightProp.Type)
+                {
+                    badJoins.Add($"{join.Source.GetJoinString()}({leftProp.Type} = {rightProp.Type})");
+                }
+            }
+
+            if (badJoins.Any())
+            {
+                var result = new RuleValidationResult(
+                        GetType().Name,
+                        $"Query '{qm.Name}' has Joins that compare incompatible types: {string.Join(", ", badJoins)}");
+
+                return result;
+            }
+            return null;
+        }
+    }
+}
