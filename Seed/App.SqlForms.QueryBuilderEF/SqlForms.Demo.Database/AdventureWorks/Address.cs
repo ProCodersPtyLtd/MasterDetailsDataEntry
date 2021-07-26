a6448778fadbb89e429a938e704ee02b18c3b0d8
Ev <ev.uklad@procoders.com.au>
2021-07-26 15:45:14 +1000

    added first seed project

---
+++ Seed/App.SqlForms.QueryBuilderEF/SqlForms.Demo.Database/AdventureWorks/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SqlForms.Demo.Database.AdventureWorks
+{
+    public partial class Address
+    {
+        public Address()
+        {
+            CustomerAddress = new HashSet<CustomerAddress>();
+            SalesOrderHeaderBillToAddress = new HashSet<SalesOrderHeader>();
+            SalesOrderHeaderShipToAddress = new HashSet<SalesOrderHeader>();
+        }
+
+        public int AddressId { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string CountryRegion { get; set; }
+        public string PostalCode { get; set; }
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ICollection<CustomerAddress> CustomerAddress { get; set; }
+        public virtual ICollection<SalesOrderHeader> SalesOrderHeaderBillToAddress { get; set; }
+        public virtual ICollection<SalesOrderHeader> SalesOrderHeaderShipToAddress { get; set; }
+    }
+}
---
+++ Seed/App.SqlForms.QueryBuilderEF/SqlForms.Demo.Database/AdventureWorks/VGetAllCategories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SqlForms.Demo.Database.AdventureWorks
+{
+    public partial class VGetAllCategories
+    {
+        public string ParentProductCategoryName { get; set; }
+        public string ProductCategoryName { get; set; }
+        public int? ProductCategoryId { get; set; }
+    }
+}
