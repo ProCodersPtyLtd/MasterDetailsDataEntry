97282c809d3457e180b2a04b2db57d4f426ad56d
Ev <ev.uklad@procoders.com.au>
2021-08-02 20:16:30 +1000

    Added dev space project

---
+++ Seed/App.SqlForms.DevSpace/SqlForms.Demo.Database/AdventureWorks/ProductDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SqlForms.Demo.Database.AdventureWorks
+{
+    public partial class ProductDescription
+    {
+        public ProductDescription()
+        {
+            ProductModelProductDescription = new HashSet<ProductModelProductDescription>();
+        }
+
+        public int ProductDescriptionId { get; set; }
+        public string Description { get; set; }
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ICollection<ProductModelProductDescription> ProductModelProductDescription { get; set; }
+    }
+}
