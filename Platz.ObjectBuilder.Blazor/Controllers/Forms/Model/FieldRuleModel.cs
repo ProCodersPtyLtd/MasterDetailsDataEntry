25a52309b71ef000c5d1094142d345a37e69f36e
Ev <ev.uklad@procoders.com.au>
2021-08-18 00:15:51 +1000

    added rules dialog

---
+++ Platz.ObjectBuilder.Blazor/Controllers/Forms/Model/FieldRuleModel.cs
@@ -0,0 +1,22 @@
+﻿using Platz.SqlForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platz.ObjectBuilder.Blazor.Model
+{
+    public class FieldRuleModel
+    {
+        public bool IsDirty { get; set; }
+        public string Name { get; set; }
+        public FormRuleTriggers Trigger { get; set; }
+        public string Code { get; set; }
+
+        public string DisplayName
+        {
+            get { return $"On{Trigger.ToString()} : {Name}"; }
+        }
+    }
+}
